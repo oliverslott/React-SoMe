6a489c3e92ee4f366c6a6ff0c64f9dd9380ccecc
betaRei <malthereippuert42@gmail.com>
2026-03-17 11:56:44 +0100

    Update frontpage.jsx

--- Frontend/src/components/ui/frontpage.jsx
+++ Frontend/src/components/ui/frontpage.jsx
@@ -66,7 +66,7 @@ function Frontpage() {
                 type="submit"
                 className="rounded-full bg-fuchsia-300 px-5 py-2.5 text-sm font-semibold text-[#2b1242] transition hover:bg-fuchsia-200"
               >
-                Slaa op
+                Slå op
               </button>
             </div>
           </form>
