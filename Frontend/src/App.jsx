a90a48d09187a7914ca47f6042019ad3ca8de383
betaRei <malthereippuert42@gmail.com>
2026-03-17 10:44:11 +0100

    added frontpage component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,27 +2,6 @@ import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import './App.css'
 
-function App() {
-  return (
-    <div className="page-shell">
-      <header className="topbar">
-        <div className="topbar__brand">SoMe</div>
-      </header>
 
-      <main className="feed-layout">
-        <section className="composer-card">
-          <h1>Del et opslag</h1>
-          <p>
-            Her kan brugere skrive opslag, som andre senere kan kommentere pa.
-          </p>
-
-          <div className="composer-placeholder">
-            Hvad har du pa hjertet i dag?
-          </div>
-        </section>
-      </main>
-    </div>
-  )
-}
 
 export default App
