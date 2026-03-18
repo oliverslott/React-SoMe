b6d2c1908d1393b108845e832d824829de7593b2
oliverslott <oliverslottsony@gmail.com>
2026-03-18 09:54:42 +0100

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from '@/components/login'
 import Register from '@/components/register'
-import Frontpage from '@/components/ui/frontpage'
+import Frontpage from '@/components/frontpage'
 
 function App() {
   return (
